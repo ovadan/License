b88a61d4f1f1aaf445a66a7a44c8856ced10ba28
ovadan <heziletdi@gmail.com>
2018-11-26 20:26:56 -0500

    Edited
    Added plate generator code

--- toString.py
+++ toString.py
@@ -2,14 +2,15 @@ import os
 from PIL import Image, ImageEnhance
 import pytesseract
 #import numpy as np
-import cv2
+#import cv2
 
-img = Image.open(os.path.expanduser("~/Desktop/py/images/sampleTH3.jpg"))
+img = Image.open(os.path.expanduser("/Users/Ovadan/Desktop/py/plate_gen/image_processed.png"))
 text = pytesseract.image_to_string(img, lang='eng')
+print(text)
 #text = text.encode('utf-8')
 
 #textEncode = text.encode('utf-8')
-print([x for x in text.split('\n')if len(x) == 7])
+#print([x for x in text.split('\n')if len(x) == 7])
 
 
 # print(text.encode('utf-8'))
