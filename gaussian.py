6e83b65dd50c52c7c13b8f0114c0f398ba7b90ea
ovadan <heziletdi@gmail.com>
2018-12-12 09:18:29 -0600

    Edited

--- gaussian.py
+++
@@ -1,7 +0,0 @@
-import os
-import cv2 as cv
-import numpy as np
-from matplotlib import pyplot as plt
-img = cv.imread(os.path.expanduser("~/Desktop/py/1235.jpg"), 0)
-th = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 7, 2)
-cv.imwrite("testing2G.jpg", th)
--- toString.py
+++ toString.py
@@ -4,13 +4,13 @@ import pytesseract
 #import numpy as np
 #import cv2
 
-img = Image.open(os.path.expanduser("/Users/Ovadan/Desktop/py/plate_gen/image_processed.png"))
+img = Image.open(os.path.expanduser("/Users/Ovadan/Desktop/py/generated_images/image_processed.png"))
 text = pytesseract.image_to_string(img, lang='eng')
-print(text)
+# print(text)
 #text = text.encode('utf-8')
 
 #textEncode = text.encode('utf-8')
-#print([x for x in text.split('\n')if len(x) == 7])
+print([x for x in text.split('\n')if len(x) == 7])
 
 
 # print(text.encode('utf-8'))
